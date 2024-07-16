c35bb1b736c37b65d20360a0613d216f76eb6b01
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-07-16 06:06:18 +0300

    Update GrossMargin.c

--- GrossMargin.c
+++ GrossMargin.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
-// Function prototypes that informs the compiler about the functions that will be used later in the program.
+// Function prototype declarations
+// These inform the compiler about the functions that will be defined later in the program.
 double calculateRevenue(double salesServicesMade, double salesProductsMade);
-double calculateGrossMargin(double Revenue, double costOfGoodsSold);
+double calculateGrossMargin(double revenue, double costOfGoodsSold);
 
-int main(){
+int main() {
+    // Variable declarations
+    double salesServicesMade, salesProductsMade, revenue, costOfGoodsSold, grossMargin;
 
-    double salesServicesMade, salesProductsMade, Revenue, costOfGoodsSold, grossMargin;
-
+    // Prompting the user to enter sales services made
     printf("Enter the sales services made: ");
     scanf("%lf", &salesServicesMade);
 
+    // Prompting the user to enter sales products made
     printf("Enter the sales products made: ");
     scanf("%lf", &salesProductsMade);
 
+    // Prompting the user to enter the cost of goods sold
     printf("Enter the cost of goods sold: ");
     scanf("%lf", &costOfGoodsSold);
 
-    Revenue = calculateRevenue(salesServicesMade, salesProductsMade);
-    grossMargin = calculateGrossMargin(Revenue, costOfGoodsSold);
+    // Calculating revenue based on sales services and products made
+    revenue = calculateRevenue(salesServicesMade, salesProductsMade);
+    // Calculating gross margin based on calculated revenue and cost of goods sold
+    grossMargin = calculateGrossMargin(revenue, costOfGoodsSold);
 
+    // Displaying the entered values
     printf("The sales services made is: %.2lf \n", salesServicesMade);
-   
     printf("The sales products made is: %.2lf \n", salesProductsMade);
-    
     printf("The cost of goods sold is: %.2lf \n", costOfGoodsSold);
 
+    // Displaying the calculated gross margin
     printf("The Gross Margin is: %.2f%% \n", grossMargin);
 
     return 0;
 }
 
-double calculateRevenue(double salesServicesMade, double salesProductsMade){
+// Function definition for calculating revenue
+// This function takes the sales services made and sales products made as inputs,
+// and returns the total revenue generated from both.
+double calculateRevenue(double salesServicesMade, double salesProductsMade) {
     return salesServicesMade + salesProductsMade;
 }
-double calculateGrossMargin(double Revenue, double costOfGoodsSold){
-    return (Revenue - costOfGoodsSold) / Revenue * 100;
+
+// Function definition for calculating gross margin
+// This function calculates the gross margin percentage by subtracting the cost of goods sold
+// from the revenue and dividing the result by the revenue, then multiplying by 100 to get a percentage.
+double calculateGrossMargin(double revenue, double costOfGoodsSold) {
+    return (revenue - costOfGoodsSold) / revenue * 100;
 }
